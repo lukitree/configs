caacd8129b5768afa741ecbb802782ed1fe04570
Edward Jenco <lukitree@gmail.com>
2016-11-14 22:27:47 -0700

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-echo Vim Config
-if [ -e "~/.vimrc" ] then
-	echo "Previous Vim configuration file exists, making a backup copy."
-	cp ~/.vimrc ~/.vimrc.bak
-fi
-cp vimrc ~/.vimrc
+function install
+{
+	echo "Installing .$1"
+	if [ -e ~/.$1 ]; then
+		echo "Previous $1 configuration file exists, making a backup copy."
+		cp ~/.$1 ~/.$1.bak
+	fi
+	cp $1 ~/.$1
+}
 
-echo ""
-echo Z-Shell Config
-if [ -e "~/.zshrc" ] then
-	echo "Previous Z-Shell configuration exists, making a backup copy."
-	cp ~/.zshrc ~/.zshrc.bak
-fi
-cp zshrc ~/.zshrc
+install vimrc
+install zshrc
 
 echo
 echo "Done!"
